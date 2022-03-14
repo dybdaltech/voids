deaf0f57b65f961e9a1bb33efa4ccb09db4898bc
Dybdatech <29250734+dybdaltech@users.noreply.github.com>
2022-03-14 12:02:36 +0100

    Movement fixed

--- game/__init__.py
+++ game/__init__.py
@@ -3,9 +3,11 @@ import pygame
 import esper
 from game.components.drawable_component import Drawable
 from game.components.Velocity_component import Velocity
+from game.components.void_component import VoidComponent, VoidType, VoidColors
 from game.processors.RenderProcessor import RenderProcessor
 from game.processors.MovementProcessor import MovementProcessor
-
+from game.processors.VoidProcessor import VoidProcessor
+from random import randint
 
 
 FPS = 60
@@ -16,7 +18,6 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(RESOLUTION)
         pygame.display.set_caption("Voids Esper")
-        print(self.window)
         self.clock = pygame.time.Clock()
         pygame.key.set_repeat(1, 1)
 
@@ -26,13 +27,19 @@ class Game:
         self.world.add_component(self.entity, Velocity(0, 0))
         self.world.add_component(self.entity, Drawable(draw_form="rect", posx=100, posy=100))
 
-        self.enemy = self.world.create_entity()
-        self.world.add_component(self.enemy, Drawable(draw_form="image", image=pygame.image.load("bluesquare.png"), posx=10, posy=100))
-
+        self.boids = []
+        for i in range(0, 100):
+            enemy = self.world.create_entity()
+            pos_x = randint(0, RESOLUTION[0])
+            pos_y = randint(0, RESOLUTION[1])
+            self.world.add_component(enemy, Drawable(draw_form="rect", posx=pos_x, posy=pos_y)) 
+            self.world.add_component(enemy, Velocity(0, 0))   
+            self.world.add_component(enemy, VoidComponent(void_type=VoidType.BOID, color=VoidColors.GREEN, x=pos_x, y=pos_y))
 
         render_processor = RenderProcessor(self.window)
         movement_processor = MovementProcessor(minx=0, maxx=RESOLUTION[0], miny=0, maxy=RESOLUTION[1])
-
+        void_processor = VoidProcessor()
+        self.world.add_processor(void_processor)
         self.world.add_processor(render_processor)
         self.world.add_processor(movement_processor)
 
--- game/processors/VoidProcessor.py
+++ game/processors/VoidProcessor.py
@@ -1,20 +1,19 @@
 import esper
 import pygame
 from math import sqrt
+from random import randint
 
 from game.components.void_component import VoidType, VoidColors, VoidComponent
 from game.components.Velocity_component import Velocity
-
+from game.processors.MovementProcessor import MovementProcessor
 
 class VoidProcessor(esper.Processor):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
 
-
-
-
-
     def process(self):
-        for ent, void in self.world.get_component(VoidComponent, Velocity):
+        for ent, void in self.world.get_component(VoidComponent):
             #TODO: Do the AI functions
-            pass
+            if self.world.has_component(ent, Velocity):
+                self.world.component_for_entity(ent, Velocity).x += randint(-5, 5)
+                self.world.component_for_entity(ent, Velocity).y += randint(-5, 5)
